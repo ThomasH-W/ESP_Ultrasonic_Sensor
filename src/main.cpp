10db05bb5fb4c55565b53d24d26c01f028aeb58b
ThomasH-W <thomas@hoeser.info>
2019-09-25 19:06:25 +0200

    1st test with newping

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
+// ---------------------------------------------------------------------------
+// This example shows how to use NewPing's ping_timer method which uses the Timer2 interrupt to get the
+// ping time. The advantage of using this method over the standard ping method is that it permits a more
+// event-driven sketch which allows you to appear to do two things at once. An example would be to ping
+// an ultrasonic sensor for a possible collision while at the same time navigating. This allows a
+// properly developed sketch to multitask. Be aware that because the ping_timer method uses Timer2,
+// other features or libraries that also use Timer2 would be effected. For example, the PWM function on
+// pins 3 & 11 on Arduino Uno (pins 9 and 11 on Arduino Mega) and the Tone library. Note, only the PWM
+// functionality of the pins is lost (as they use Timer2 to do PWM), the pins are still available to use.
+// NOTE: For Teensy/Leonardo (ATmega32U4) the library uses Timer4 instead of Timer2.
+// ---------------------------------------------------------------------------
 #include <Arduino.h>
+#include <NewPing.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define TRIGGER_PIN D7    // D7   // Arduino pin tied to trigger pin on ping sensor.
+#define ECHO_PIN D6       // Arduino pin tied to echo pin on ping sensor.
+#define MAX_DISTANCE 2000 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+void setup()
+{
+  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  delay(100); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  Serial.print("Ping: ");
+  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  Serial.println("cm");
 }
